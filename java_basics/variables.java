09b3b45e7dc641b667de6b27db2a9d2147735b3c
pratheeksha29-20 <pratheekshadevaraj.phd.123@gmail.com>
2026-08-03 14:28:38 +0530

    variables of java_basics done

---
+++ java_basics/variables.java
@@ -0,0 +1,12 @@
+
+package java_oops_dsa.java_basics;
+import java.util.*;
+public class variables {
+public static void main(String[]args){
+    int a = 10;
+    float b = 20.00f;
+    String c ="hii hello";
+    double d = 20.54;
+    System.out.println("the value of a , b , c , d :"+a+" "+b+" "+c+" "+d);
+}    
+}
