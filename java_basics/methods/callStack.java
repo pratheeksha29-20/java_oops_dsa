92f75500dd72ee96915825e33379e06297389aa8
pratheeksha29-20 <pratheekshadevaraj.phd.123@gmail.com>
2026-08-08 05:10:33 +0530

    callstack of method done in java basics

---
+++ java_basics/methods/callStack.java
@@ -0,0 +1,79 @@
+package java_oops_dsa.java_basics.methods;
+
+public class callStack {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main Starts");
+
+        first();
+
+        System.out.println("Main Ends");
+    }
+
+    public static void first() {
+
+        System.out.println("First Method");
+
+        second();
+
+        System.out.println("Back to First");
+    }
+
+    public static void second() {
+
+        System.out.println("Second Method");
+    }
+}
+
+/*Step 1
+
+---------
+| main  |
+---------
+
+----------------------------
+
+Step 2
+
+---------
+| first |
+---------
+| main  |
+---------
+
+----------------------------
+
+Step 3
+
+---------
+|second |
+---------
+| first |
+---------
+| main  |
+---------
+
+----------------------------
+
+Step 4
+
+---------
+| first |
+---------
+| main  |
+---------
+
+----------------------------
+
+Step 5
+
+---------
+| main |
+---------
+
+----------------------------
+
+Program Ends
+
+Stack Empty*/
